d37f359096da19a05ecd7aebc4e2baae55c98f9a
oluwatoyinGFA <oluwatoyin@gfa-tech.com>
2026-04-23 10:21:35 +0100

    updated

--- portal/app/Views/loadSoftsKillsAnalyticsOld.php
+++ portal/app/Views/loadSoftsKillsAnalyticsOld.php
@@ -2,21 +2,23 @@
   $this->gfa_model = model('App\Models\GfaModel');
   $email = session()->get('email');
   // $loginkey = $this->gfa_model->getWpCred($email);
-  // $cohort = $this->gfa_model->getCohortDetails($email);
-  // if($cohort > 2){
-	  $courseTrack = $this->gfa_model->GetUserProgressSoftCurriculumWema($email);
-  // } else {
-  // 	$courseTrack = $this->gfa_model->GetUserProgressAssignedCoursesWema($email);
-  // }
-        
+  $courseTrack = $this->gfa_model->GetUserProgressSoftCurriculumWema($email);
+  // $courseTrack = $this->gfa_model->GetUserProgressSoftSkills($email);
+// $courseTrack2 = $this->gfa_model->GetUserQuizProgressSoftSkill($email);
+// if ($courseTrack[0]['NumberOfCourses'] >= $courseTrack2[0]['NumberOfCourses']) {
+// 	$courseTrack = $this->gfa_model->GetUserProgressSoftSkills($email);
+// } else {
+// 	$courseTrack = $this->gfa_model->GetUserQuizProgressSoftSkill($email);
+// }
+#print_r($courseTrack);
 ?>
 <div class="col-sm-6 col-xl-3">
     <div class="card">
       <div class="card-body">
         <div class="d-flex align-items-center justify-content-between">
           <div class="content-left">
-            <h4 class="mb-0"><?php echo $courseTrack[0]['TotalCourses']; ?></h4>
-            <small>Total Courses</small>
+            <h4 class="mb-0"><?php echo $courseTrack[0]['NumberOfCourses']; ?></h4>
+            <small>Completed Courses</small>
           </div>
           <span class="badge bg-label-primary rounded-circle p-2">
             <i class="ti ti-book ti-md"></i>
@@ -43,17 +45,17 @@
   <!-- <div class="col-sm-6 col-xl-3">
     <div class="card">
       <div class="card-body">
-        <a href="<?php echo base_url("gfa/lesson_progress/{$courseTrack[0]['OngoingCourse']}"); ?>" class="stretched-link" style="font-weight: bold; color: #5c5460;">
-          <div class="d-flex align-items-center justify-content-between">
-            <div class="content-left">
-              <h6 class="mb-0"><?php echo $courseTrack[0]['OngoingCourse'] ?></h6>
-              <small style="color: green;">Current Course</small>
-            </div>
-            <span class="badge bg-label-danger rounded-circle p-2">
-              <i class="ti ti-user ti-md"></i>
-            </span>
+<a href="<?php echo base_url("gfa/lesson_progress/{$courseTrack[0]['Ongoing']}"); ?>" class="stretched-link" style="font-weight: bold; color: #5c5460;">
+        <div class="d-flex align-items-center justify-content-between">
+          <div class="content-left">
+            <h6 class="mb-0"><?php echo $courseTrack[0]['Ongoing'] ?></h6>
+            <small style="color: green;">Current Course</small>
           </div>
-        </a>
+          <span class="badge bg-label-danger rounded-circle p-2">
+            <i class="ti ti-user ti-md"></i>
+          </span>
+        </div>
+</a>
       </div>
     </div>
   </div> -->
@@ -62,7 +64,7 @@
       <div class="card-body">
         <div class="d-flex align-items-center justify-content-between">
           <div class="content-left">
-            <h4 class="mb-0"><?php echo $courseTrack[0]['PassedQuizzes'] ?></h4>
+            <h4 class="mb-0"><?php echo $courseTrack[0]['NumberOfPassedQuizzes'] ?></h4>
             <a href="<?php echo base_url("gfa/quiz_progress/{$courseTrack[0]['OngoingCourse']}"); ?>" class="stretched-link" style="font-weight: bold; color: #1731c3;">
              <small>Passed Quiz</small>
             </a>
@@ -74,19 +76,19 @@
       </div>
     </div>
   </div>
-  
-  <div class="col-sm-6 col-xl-3">
+
+<div class="col-sm-6 col-xl-3">
     <div class="card">
       <div class="card-body">
         <div class="d-flex align-items-center justify-content-between">
           <div class="content-left">
             <h4 class="mb-0">Certificate</h4>
-            <small>Access Your Technical Skill Certificate</small>
+            <small>Access Your Soft Skills Certificate</small>
           </div>
-          <?php $cert_course_ref  = session()->get('cert_course_ref') ; if(!empty($cert_course_ref)){ ?>
-          <a href="<?php echo base_url("gfa/certificate_dimp_skills/{$cert_course_ref}"); ?>" class="btn bg-label-danger">
+          <?php $cert_soft_ref  = session()->get('cert_soft_ref') ; if(!empty($cert_soft_ref)){ ?>
+          <a href="<?php echo base_url("gfa/certificate_soft_skills/{$cert_soft_ref}"); ?>" class="btn bg-label-danger">
           <?php }else{  ?>
-            <a href="<?php echo base_url("gfa/certificate_gen_dimp/"); ?>" class="btn bg-label-danger"> 
+            <a href="<?php echo base_url("gfa/certificate_gen/"); ?>" class="btn bg-label-danger"> 
             <?php }  ?> 
           <i class="ti ti-link ti-md"></i>
           </a>
@@ -94,4 +96,22 @@
       </div>
     </div>
   </div>
+
+  <!-- <div class="col-sm-6 col-xl-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex align-items-center justify-content-between">
+          <div class="content-left">
+			<a href="<?php echo base_url("gfa/updateCertificateNameView"); ?>" class="btn bg-label-danger">
+            	<h6 class="mb-0">Update Your Certificate Name</h6>
+			</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+  
+  
+  
   
