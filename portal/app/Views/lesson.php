40202f1dc616af098423e96e2dd456546a37c639
oluwatoyinGFA <oluwatoyin@gfa-tech.com>
2026-04-29 18:48:39 +0100

    updated

--- portal/app/Views/lesson.php
+++ portal/app/Views/lesson.php
@@ -116,7 +116,13 @@ if ($position !== false) {
             <?php }  ?>
             <?php if($getQuizByLessonIdData[0]['lesson_id'] ==''){  echo ''; }else{ ?>
             <a href="<?php echo base_url("gfa/quiz/{$getActiveQuizData[0]['ref_id']}") ?>" class="btn rounded-pill btn-secondary">Quiz</a>
-            <?php }  ?>
+            <?php } 
+            
+              $getActiveQuizData = $this->gfa_model->getQuizBySectionId($getActiveLessonData[0]['section_id']);
+        			// foreach($getActiveQuizData as $getActiveQuiz){     
+            ?>
+            <a href="<?php echo base_url("gfa/quiz/{$getActiveQuizData[0]['ref_id']}") ?>" ls="<?= 'q-' . $getActiveQuizData[0]['ref_id'];  ?>" class="btn rounded-pill btn-secondary userActivity">Quiz</a>
+
           </div>
           <hr class="my-4">
           <h5 class="mb-2">About this lesson</h5>
