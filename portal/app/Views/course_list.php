460df131e4433383c2ef547319cb1cfab4439cd7
oluwatoyinGFA <oluwatoyin@gfa-tech.com>
2026-05-21 17:52:25 +0100

    updated

--- portal/app/Views/course_list.php
+++ portal/app/Views/course_list.php
@@ -112,7 +112,7 @@
               <div class="d-flex justify-content-between align-items-center mb-3">
                 <?php                
                  if ($this->gfa_model->checkCompletionSingleCourse($email, $courseDetailsRec['id'])) {
-                    echo '<span class="badge bg-danger text-white">Completed</span>';
+                    echo '<span class="badge bg-danger text-white">Passed</span>';
 					        }
                  ?>
                 <!-- <span class="badge bg-success">Duration: <ls style="color:#"><?php echo $courseDetailsRec['duration']; ?> <?php echo $courseDetailsRec['duration_time']; ?></ls></span> -->
