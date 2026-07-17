4dfe216a7ac9300f4b87db4feebc7f33b70e2c2c
oluwatoyinGFA <oluwatoyin@gfa-tech.com>
2026-07-17 18:10:30 +0100

    updated encryption in database.php file

--- portal/app/Config/Database.php
+++ portal/app/Config/Database.php
@@ -38,7 +38,11 @@ class Database extends Config
         'charset'  => 'utf8',
         'DBCollat' => 'utf8_general_ci',
         'swapPre'  => '',
-        'encrypt'  => false,
+        'encrypt' => [
+            'ssl_ca'     => null, 
+            'ssl_verify' => false, 
+        ],
+        // 'encrypt'  => false,
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
