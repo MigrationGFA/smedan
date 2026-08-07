c9f0c2ce04901de4506079c1a102d4e310e64e5e
oluwatoyinGFA <oluwatoyin@gfa-tech.com>
2026-08-07 08:55:51 +0100

    updated

--- portal/app/Controllers/Gfa.php
+++ portal/app/Controllers/Gfa.php
@@ -6352,9 +6352,9 @@ $data_connection = array(
         // 1. Check user first BEFORE setting session/cookie
         $existingUser = $this->gfa_model->checkWemaUser($email);
 
-        if ($existingUser) {
-            $created    = strtotime($existingUser['date']);
-            $daysPassed = (time() - $created) / 86400;
+        // if ($existingUser) {
+        //     $created    = strtotime($existingUser['date']);
+        //     $daysPassed = (time() - $created) / 86400;
 
             // if ($daysPassed > 30) {
             //     // Expired — clear everything and redirect
@@ -6364,7 +6364,7 @@ $data_connection = array(
             //     delete_cookie('last_name');
             //     return redirect()->to('https://smedan.remsana.com');
             // }
-        }
+        // }
 
         // 2. Access valid — now set session and cookie
         session()->set([
